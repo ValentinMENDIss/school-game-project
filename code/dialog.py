10d59752c36cfdd1aee6ab7f247e00ee9144b53e
ValentinMENDIss <113135642+ValentinMENDIss@users.noreply.github.com>
2024-12-26 16:33:55 +0100

    Added more comments to the code

--- code/dialog.py
+++ code/dialog.py
@@ -4,8 +4,16 @@ from entities import *
 
 ######### CLASSES ##############
 
-class Dialog():
+class Dialog:
     def __init__(self):
-        pass
+        self.SCREEN = pygame.display.get_surface()  # initializing screen (SCREEN)
+
     def interact(self,text):
+        smalltext = pygame.font.Font(os.path.join('..', 'font', 'Pixeltype.ttf'),20)  # Set Font and Size for the Small Text
+        smalltext = smalltext.render(text, True,(0, 0, 0)).convert_alpha()  # Render a Small Text
+        smalltextrect = smalltext.get_rect()  # Get a Rectangle of the small Text ( needed, to be able to place the Text precisely )
+        smalltextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 30)  # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
+
+        self.SCREEN.blit(smalltext, smalltextrect)  # Draw a Text on the Screen
+
         print(text)
--- code/main.py
+++ code/main.py
@@ -12,75 +12,77 @@ from dialog import *
 
 class Game:
     def __init__(self):
+        # INITIALIZE FRAMEWORK
+        pygame.init()                                                                                                   # initialize pygame framework
+        pygame.font.init()                                                                                              # initialize pygame text/font framework
 
-        pygame.init()                                                                               # initialize pygame framework
-        pygame.font.init()
-        self.SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))                        # create screen with (x,y) (tuple)
-        pygame.display.set_caption("School-Game-Project(11. Grade)")                                # set/change title (caption) of the window
-        self.clock = pygame.time.Clock()                                                            # create a clock
-        self.ticks = pygame.time.get_ticks()
+        # INITIALIZE VARIABLES
+        self.interact = False                                                                                           # declare/initialize self.interact variable that has a default value: False
+        self.dialog = None                                                                                              # declare self.dialog variable that has no value in it (needed because else Dialog() will not be set up properly for some reason)
+
+        # CONFIGURING PYGAME
+        self.SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))                                            # create screen with (x,y) (tuple)
+        pygame.display.set_caption("School-Game-Project(11. Grade)")                                                    # set/change title (caption) of the window
+        self.clock = pygame.time.Clock()                                                                                # create a clock
+        self.ticks = pygame.time.get_ticks()                                                                            # get ticks (needed in order to count how much time is gone)
 
 
         # GROUPS
-        self.all_sprites = AllSprites()                                                             # create a sprite group # assigns to AllSprites() Class
+        self.all_sprites = AllSprites()                                                                                 # create a sprite group # assigns to AllSprites() Class
         
-        self.import_assets()                                                                        # import tilesets (assets)
-        self.setup(self.tmx_maps['world'], 'spawn')                                   # import this specific one tileset (mapset/asset)
+        self.import_assets()                                                                                            # import tilesets (assets)
+        self.setup(self.tmx_maps['world'], 'spawn')                                                       # import this specific one tileset (mapset/asset)
         
     def import_assets(self):
-        self.tmx_maps = {'world': load_pygame(os.path.join('..', 'data', 'maps', 'world.tmx'))}     # load world.tmx file (with given location of it)
+        self.tmx_maps = {'world': load_pygame(os.path.join('..', 'data', 'maps', 'world.tmx'))}                         # load world.tmx file (with given location of it)
         
     def setup(self, tmx_map, player_start_pos):
-        for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles():                              # get only 'Terrain' layer from world.tmx
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)                     # parse information of sprite to Sprite() class
+        for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles():                                                  # get only 'Terrain' layer from world.tmx
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)                                         # parse information of sprite to Sprite() class
 
         for obj in tmx_map.get_layer_by_name('Entities'):
-            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
-                self.player = Player((obj.x, obj.y), self.all_sprites)
+            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:                                      # check whether the object's name is Player and its properties for pos(position). Check also whether it is the same as player_start_pos
+                self.player = Player((obj.x, obj.y), self.all_sprites)                                             # create player() instance with object's x and y coordinates that we got from tilemap(tmx). And assign player() instance to AllSprites() group/class
             if obj.name == 'Character' and obj.properties['pos'] == 'bottom-right':
                 self.npc = NPC((obj.x, obj.y), self.all_sprites)
 
-    def text(self, text):
-        smallText = pygame.font.Font(os.path.join('..', 'font', 'Pixeltype.ttf'),20)  # Set Font and Size for the Small Text
-        smallText = smallText.render(text, True,(0, 0, 0)).convert_alpha()  # Render a Small Text
-        smallTextRect = smallText.get_rect()  # Get a Rectangle of the small Text ( needed, to be able to place the Text precisely )
-        smallTextRect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 30)  # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
-
-        self.SCREEN.blit(smallText, smallTextRect)  # Draw a Text on the Screen
-
-    # dialog system
+    # DIALOG SYSTEM
     def input(self):
-        keys = pygame.key.get_just_pressed()
-        if keys[pygame.K_e]:
-            self.text("YIPPEEE!!!")
-            self.dialog = Dialog()
-            self.dialog.interact("Hello World")
+        keys = pygame.key.get_just_pressed()                                                                            # initialize new varable(keys) that will get user's input, but the buttons can be detected as pressed and not as hold too.
+        if keys[pygame.K_e]:                                                                                            # if just pressed key is e do following:
+            self.interact = True                                                                                        # assign following value to self.interact variable: True
 
-            # HOW TO IMPLEMENT TIMER FOR TEXT (IN ORDER FOR TEXT TO APPEAR ON THE SCREEN FOR 10 SECONDS): https://stackoverflow.com/questions/30720665/countdown-timer-in-pygame#30728904
 
     def run(self):
+        # PYGAME EVENTS
         while True:
-            dt = self.clock.tick() / 1000                                                           # tick every second  # dt = difference between previous and next frame
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
+            dt = self.clock.tick() / 1000                                                                               # tick every second  # dt = difference between previous and next frame
+            for event in pygame.event.get():                                                                            # for every single event that is available in pygame do following:
+                if event.type == pygame.QUIT:                                                                           # if event type is 'QUIT' do following:
+                    pygame.quit()                                                                                       # quit/exit
 
-            # pygame logic
+            # PYGAME LOGIC
             self.all_sprites.update(dt)                                                                                 # update screen (all sprites) by FPS
             self.SCREEN.fill('white')                                                                                   # fill screen with white color, so it's fully updated
             self.all_sprites.draw(self.player.rect.center)                                                              # draw all sprites to the center of the rectangle of the player (camera)
-            self.text("Made as a school project, Have fun :) (TEST FRAMEWORK: TEXT)")
-            self.input()
+            self.input()                                                                                                # take user's input
 
+            # CHECK-CONDITIONS
+            if self.dialog == None:                                                                                     # check whether self.dialog variable is assigned to nothing
+                self.dialog = Dialog()                                                                                  # assign self.dialog variable to Dialog() class
+            if self.interact == True:
+                self.dialog.interact("Hello World")                                                                     # interact with npc, text in speech bubble
+
+            #self.dialog.interact("YIPPEEE :3")
 
             pygame.display.update()                                                                                     # refresh(update) the screen
 
-            #print((pygame.time.get_ticks() - self.ticks) / 1000)                                                                 # calculate how many seconds have passed
+            #print((pygame.time.get_ticks() - dt) / 1000)                                                               # calculate how many seconds have passed
 
 
 
 ####### MAIN CODE ############
 
-if __name__ == "__main__":                                                                          # if code is in main.py (__main__) run following
-    game = Game()
-    game.run()                                                                                      # run game (game-logic)
+if __name__ == "__main__":                                                                                              # if code is in main.py (__main__) run following
+    game = Game()                                                                                                       # initialize game() (class)
+    game.run()                                                                                                          # run game (game-logic)
