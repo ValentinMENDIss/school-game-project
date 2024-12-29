036c5b308e2cecc97cbf7fe2a545b9cc62670bdd
ValentinMENDIss <valiksuper58@gmail.com>
2024-12-29 20:23:33 +0100

    Fixed Errors Bug after exiting the game

--- code/main.py
+++ code/main.py
@@ -32,18 +32,18 @@ class Game:
         self.all_sprites = AllSprites()                                                                                 # create a sprite group # assigns to AllSprites() Class
         
         self.import_assets()                                                                                            # import tilesets (assets)
-        self.setup(self.tmx_maps['world'], 'spawn')                                                       # import this specific one tileset (mapset/asset)
+        self.setup(self.tmx_maps['world'], 'spawn')                                                                     # import this one specific tileset (mapset/asset)
         
     def import_assets(self):
         self.tmx_maps = {'world': load_pygame(os.path.join('..', 'data', 'maps', 'world.tmx'))}                         # load world.tmx file (with given location of it)
         
     def setup(self, tmx_map, player_start_pos):
         for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles():                                                  # get only 'Terrain' layer from world.tmx
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)                                         # parse information of sprite to Sprite() class
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)                                              # parse information of sprite to Sprite() class
 
         for obj in tmx_map.get_layer_by_name('Entities'):
             if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:                                      # check whether the object's name is Player and its properties for pos(position). Check also whether it is the same as player_start_pos
-                self.player = Player((obj.x, obj.y), self.all_sprites)                                             # create player() instance with object's x and y coordinates that we got from tilemap(tmx). And assign player() instance to AllSprites() group/class
+                self.player = Player((obj.x, obj.y), self.all_sprites)                                                  # create player() instance with object's x and y coordinates that we got from tilemap(tmx). And assign player() instance to AllSprites() group/class
             if obj.name == 'Character' and obj.properties['pos'] == 'bottom-right':
                 self.npc = NPC((obj.x, obj.y), self.all_sprites)
 
@@ -56,21 +56,25 @@ class Game:
 
 
     def run(self):
+        # VARIABLES
+        self.run = True                                                                                                 # initializing variable for main loop
+
         # PYGAME EVENTS
-        while True:
-            dt = self.clock.tick() / 1000                                                                               # tick every second  # dt = difference between previous and next frame
+        while self.run == True:
             for event in pygame.event.get():                                                                            # for every single event that is available in pygame do following:
                 if event.type == pygame.QUIT:                                                                           # if event type is 'QUIT' do following:
-                    pygame.quit()                                                                                       # quit/exit
+                    self.run = False                                                                                    # quit/exit by assigning boolean 'False' to self.run variable
 
             # PYGAME LOGIC
+            dt = self.clock.tick() / 1000                                                                               # tick every second  # dt = difference between previous and next frame
             self.all_sprites.update(dt)                                                                                 # update screen (all sprites) by FPS
             self.SCREEN.fill('white')                                                                                   # fill screen with white color, so it's fully updated
             self.all_sprites.draw(self.player.rect.center)                                                              # draw all sprites to the center of the rectangle of the player (camera)
             self.input()                                                                                                # take user's input
 
+            # INTERACTION HANDLING
             if self.interact == True:                                                                                   # check whether interact condition is true or not (bool check)
-                self.npc.interact("OMG, I CAN SPEAK!!! Thank you developers :3", self.player.rect)                 # interact with npc, text in speech bubble
+                self.npc.interact("OMG, I CAN SPEAK!!! Thank you developers :3", self.player.rect)                      # interact with npc, text in speech bubble
                 if self.interact_start_time == 0:                                                                       # if interact start time equals to 0, do following:
                     self.interact_start_time = pygame.time.get_ticks()                                                  # assign ticks to interact start time variable
                 elif pygame.time.get_ticks() - self.interact_start_time >= self.interact_duration:                      # else if more or equal time than interact duration has been gone do following:
