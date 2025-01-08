1ad85421203bfcbdacf50b597076cd2f7972db28
ValentinMENDIss <113135642+ValentinMENDIss@users.noreply.github.com>
2025-01-08 17:42:03 +0100

    Added Settings Menu and new Button Images

--- code/menu.py
+++ code/menu.py
@@ -7,28 +7,11 @@ from input import *
 class Menu:
     def __init__(self):
         self.text = ""
+        self.start_button = Button(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, START_IMG, 0.8)  # create button instance
+        self.exit_button = Button(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 125, EXIT_IMG, 0.8)  # create button instance
+        self.settings_input_button = Button(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 250, SETTINGS_IMG, 0.8)
+        self.return_button = Button(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 250, RETURN_IMG, 0.8)
 
-    def draw(self, surface):
-        # SETTING TEXT FOR MENU
-        self.heading_text = "Menu"
-
-        # DEFINING TEXT VARIABLES
-
-        self.headingtext = HEADINGTEXT.render(self.heading_text, True, (0, 0, 0)).convert_alpha()  # render a Small Text
-        self.headingtextrect = self.headingtext.get_rect()                                                              # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
-        self.headingtextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 250)                                     # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
-
-
-        # DRAWING TO SURFACE
-        surface.fill((187, 180, 207))                                                                                   # Draw a white coloured Screen
-        surface.blit(self.headingtext, self.headingtextrect)
-
-        self.start_button = Button(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, START_IMG, 0.8)                         # create button instance
-        self.start_button.draw(surface)
-        self.exit_button = Button(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 125, EXIT_IMG, 0.8)                     # create button instance
-        self.exit_button.draw(surface)
-
-        pygame.display.update()                                                                                         # update the screen
 
     def show(self, surface, input):
         self.running = True
@@ -36,20 +19,81 @@ class Menu:
         self.input = input
 
         while self.running:
-            self.draw(surface)
-            if self.start_button.action:
-                pygame.mixer.Sound.play(MENU_SOUND)
-                pygame.mixer.music.stop()
+            self.main_menu(surface)
+
+            if self.exit_action:
                 self.running = False
-            if self.exit_button.action:
-                self.exit_action = True
-                return self.exit_action
+
+    def events(self):
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.running = False
+
             self.input.menu()
             if self.input.menu_running == False:
                 self.running = False
+
+    def main_menu(self, surface):
+        running = True
+        while running:
+            # SETTING TEXT FOR MENU
+            heading_text = "Menu"
+            # DEFINING TEXT VARIABLES
+
+            headingtext = HEADINGTEXT.render(heading_text, True, (0, 0, 0)).convert_alpha()  # render a Small Text
+            headingtextrect = headingtext.get_rect()                                                              # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
+            headingtextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 250)                                     # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
+
+
+            # DRAWING TO SURFACE
+            surface.fill((187, 180, 207))                                                                                   # Draw a white coloured Screen
+            surface.blit(headingtext, headingtextrect)
+
+            self.start_button.draw(surface)
+
+            self.exit_button.draw(surface)
+
+            self.settings_input_button.draw(surface)
+
+            if self.start_button.action:
+                pygame.mixer.Sound.play(MENU_SOUND)
+                pygame.mixer.music.stop()
+                self.running, running = False, False
+            if self.exit_button.action:
+                self.exit_action = True
+                return self.exit_action
+            if self.settings_input_button.action:
+                self.settings_input(surface)
+                running = False
+
+            self.events()
+
+            pygame.display.update()                                                                                         # update the screen
+
+
+
+    def settings_input(self, surface):
+        running = True
+        while running:
+            # SETTING TEXT FOR MENU
+            heading_text = "Settings - Input"
+
+            # DEFINING TEXT VARIABLES
+            headingtext = HEADINGTEXT.render(heading_text, True, (0, 0, 0)).convert_alpha()  # render a Small Text
+            headingtextrect = headingtext.get_rect()  # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
+            headingtextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 250)  # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
+
+            # DRAWING TO SURFACE
+            surface.fill((187, 180, 207))  # Draw a white coloured Screen
+            surface.blit(headingtext, headingtextrect)
+
+            self.return_button.draw(surface)
+
+            if self.return_button.action:
+                self.main_menu(surface)
+                running = False
+
+            self.events()
+
+            pygame.display.update()  # update the screen
