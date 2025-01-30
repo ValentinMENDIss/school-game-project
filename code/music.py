dfb9cdd0c3e4a94263516cd2c040b54e3f95bb76
ValentinMENDIss <113135642+ValentinMENDIss@users.noreply.github.com>
2025-01-30 21:54:16 +0100

    added fade-in/fade-out for random music play function

--- code/music.py
+++ code/music.py
@@ -1,19 +1,28 @@
 from settings import *
+from timer import Timer
 
 class Music:
     def __init__(self):
-        self.paused = False
         pygame.mixer.music.set_volume(0.35)
+        self.timer = Timer()
+        self.paused = False
+        self.fade = 5000                                                                                # music's fade-in/fade-out (in ms)
+
     def play(self, music):
         pygame.mixer.music.stop()
         pygame.mixer.music.load(music)
         pygame.mixer.music.play()                                                                     # The '-1' means repeat endlessly
 
     def play_random(self):
-        pygame.mixer.music.stop()
-        index = random.randint(0, len(MUSIC) - 1)
-        pygame.mixer.music.load(MUSIC[index])
-        pygame.mixer.music.play()
+        if self.timer.active == False and not self.timer.is_finished:
+            self.timer.start(self.fade)
+        if self.timer.is_finished:
+            self.timer.is_finished = False
+            pygame.mixer.music.stop()
+            index = random.randint(0, len(MUSIC) - 1)
+            pygame.mixer.music.load(MUSIC[index])
+            pygame.mixer.music.play()
+        self.timer.update()
 
     def check_status(self):
         if pygame.mixer.music.get_busy():
