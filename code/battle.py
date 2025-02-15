55964bd65cdf2fc3eaf8a1a728bac7e0db9091ab
ValentinMENDIss <valiksuper58@gmail.com>
2025-02-15 20:13:33 +0100

    Updated Battle Menu drawing system + Blank Items Menu

--- code/battle.py
+++ code/battle.py
@@ -18,6 +18,7 @@ class Battle_Menu:
     def __init__(self, enemy_health, game):
         self.game = game                                                                                                                    # declare new variable that stores main.py (Game()) loop's attributes and variables
         self.attack_type = ["attack", "emotional_attack"]                                                                                   # declare attack types
+        self.current_display = "battle_menu"
         self.get_pressed_keys_action = False                                                                                                # variable that stores Bool value. Checks if action should be called
         self.exit_action = False                                                                                                            # variable that stores Bool value. Checks if action should be called
         self.show_player_damage_action = False                                                                                              # variable that stores Bool value. Checks if action should be called
@@ -25,6 +26,12 @@ class Battle_Menu:
         self.enemy_health = enemy_health                                                                                                    # enemy's health
         self.player_health = self.game.player.health                                                                                        # player's health
         self.damage = 0                                                                                                                     # variable that stores dealt damage in one round
+        self.is_running = True
+        self.FightButtonMenu = False
+        # ACTION
+        self.action = None
+        self.action_start_time = 0
+        self.action_duration = 2000
         # IMAGES
         self.enemy_image = NPC_IDLE.convert_alpha()
         self.enemy_new_size_image = (self.enemy_image.get_width() * 4, self.enemy_image.get_height() * 4)                                 # declare new variable that has 4 times bigger scale than the player's image
@@ -32,6 +39,8 @@ class Battle_Menu:
         # TEXTs
         self.enemytext = ""
         self.damagetext = ""
+        # DEFINING CLASS VARIABLES
+        self.timer = Timer()
 
     def random_text(self):
         if self.enemy_health <= 0:
@@ -87,141 +96,171 @@ class Battle_Menu:
 
     # DRAWING LOGIC
     def draw(self, surface):
-        FightButtonMenu = None
-        # ACTION
-        action = None
-        action_start_time = 0
-        action_duration = 2000
-        # LOOP VARIABLES
-        self.running = True
+        self.game.music.pause()
+        while self.is_running:
+            if self.current_display == "battle_menu":
+                self.battle_menu(surface)
+            elif self.current_display == "items_menu":
+                self.items_menu(surface)
 
+    def battle_menu(self, surface):
         # INITIALIZING BUTTONS
         SURRENDER_BUTTON = Button(WINDOW_WIDTH - 150, WINDOW_HEIGHT - 100, scale=0.40, image=SURRENDER_IMG, hovered_image=SURRENDER_IMG_PRESSED)                             # create button instance
         FIGHT_BUTTON = Button(150, WINDOW_HEIGHT - 100, scale=0.40, image=START_IMG, hovered_image=START_IMG_PRESSED)
         DEFENSE_BUTTON = Button(400, WINDOW_HEIGHT - 100, scale=0.40, image=START_IMG, hovered_image=START_IMG_PRESSED)
         ITEMS_BUTTON = Button(650, WINDOW_HEIGHT - 100, scale=0.40, image=START_IMG, hovered_image=START_IMG_PRESSED)
         SPELL_BUTTON = Button(900, WINDOW_HEIGHT - 100, scale=0.40, image=START_IMG, hovered_image=START_IMG_PRESSED)
-
         EMOTIONAL_ATTACK_BUTTON = Button(150, WINDOW_HEIGHT - 175, scale=0.40, image=START_IMG, hovered_image=START_IMG_PRESSED)
         ATTACK_BUTTON = Button(150, WINDOW_HEIGHT - 250, scale=0.40, image=START_IMG, hovered_image=START_IMG_PRESSED)
 
-        # SETTING TEXT FOR MENU
+        # DEFINING TEXT VARIABLES
         heading_text = "Battle Menu"
-        timer = Timer()
-        self.game.music.pause()
-       # LOOP
-        while self.running:
-            # DEFINING CONSTANT VARIABLES
-            MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-            # DEFINING TEXT VARIABLES
-            headingtext = HEADINGTEXT.render(heading_text, True, (0, 0, 0)).convert_alpha()  # render a Small Text
-            headingtextrect = headingtext.get_rect()                                                              # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
-            headingtextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 250)                                     # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
+        headingtext = HEADINGTEXT.render(heading_text, True, (0, 0, 0)).convert_alpha()  # render a Small Text
+        headingtextrect = headingtext.get_rect()                                                              # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
+        headingtextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 250)                                     # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
 
-            enemytext = SMALLTEXT.render(self.enemytext, True, (0, 0, 0)).convert_alpha()        # render an enemy text
-            enemytextrect = enemytext.get_rect()
-            enemytextrect.center = (WINDOW_WIDTH // 2 + 350, WINDOW_HEIGHT // 2 - 100)
+        enemytext = SMALLTEXT.render(self.enemytext, True, (0, 0, 0)).convert_alpha()        # render an enemy text
+        enemytextrect = enemytext.get_rect()
+        enemytextrect.center = (WINDOW_WIDTH // 2 + 350, WINDOW_HEIGHT // 2 - 100)
 
-            playerhealthtext = HEADINGTEXT.render(str(self.player_health), True, (4,106,56)).convert_alpha()        # render an enemy text
-            playerhealthtextrect = playerhealthtext.get_rect()
-            playerhealthtextrect.center = (450, 500)
+        playerhealthtext = HEADINGTEXT.render(str(self.player_health), True, (4,106,56)).convert_alpha()        # render an enemy text
+        playerhealthtextrect = playerhealthtext.get_rect()
+        playerhealthtextrect.center = (450, 500)
 
-            enemyhealthtext = HEADINGTEXT.render(str(self.enemy_health), True, (4,106,56)).convert_alpha()        # render an enemy text
-            enemyhealthtextrect = enemyhealthtext.get_rect()
-            enemyhealthtextrect.center = (WINDOW_WIDTH // 2 + 350, 150)
+        enemyhealthtext = HEADINGTEXT.render(str(self.enemy_health), True, (4,106,56)).convert_alpha()        # render an enemy text
+        enemyhealthtextrect = enemyhealthtext.get_rect()
+        enemyhealthtextrect.center = (WINDOW_WIDTH // 2 + 350, 150)
 
-            # DRAWING ON THE SURFACE
-            surface.blit(BACKGROUND_IMG)
-            surface.blit(headingtext, headingtextrect)
-            surface.blit(enemytext, enemytextrect)
-            surface.blit(playerhealthtext, playerhealthtextrect)
-            surface.blit(enemyhealthtext, enemyhealthtextrect)
-            surface.blit(self.enemy_image, (WINDOW_WIDTH // 2 + 280, WINDOW_HEIGHT // 2 - 70))
+        # DEFINING CONSTANT VARIABLES
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-            ## DRAWING BUTTONS ##
-            for button in [SURRENDER_BUTTON, FIGHT_BUTTON, DEFENSE_BUTTON, ITEMS_BUTTON, SPELL_BUTTON]:                     # iterate through every single button instance and draw it to the screen
-                button.draw(surface)
+        # DRAWING ON THE SURFACE
+        surface.blit(BACKGROUND_IMG)
+        surface.blit(headingtext, headingtextrect)
+        surface.blit(enemytext, enemytextrect)
+        surface.blit(playerhealthtext, playerhealthtextrect)
+        surface.blit(enemyhealthtext, enemyhealthtextrect)
+        surface.blit(self.enemy_image, (WINDOW_WIDTH // 2 + 280, WINDOW_HEIGHT // 2 - 70))
 
-            if FightButtonMenu:
-                EMOTIONAL_ATTACK_BUTTON.draw(surface)
-                ATTACK_BUTTON.draw(surface)
-            ## EVENTS ##
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:                                                                               # exit game function
+        ## DRAWING BUTTONS ##
+        for button in [SURRENDER_BUTTON, FIGHT_BUTTON, DEFENSE_BUTTON, ITEMS_BUTTON, SPELL_BUTTON]:                     # iterate through every single button instance and draw it to the screen
+            button.draw(surface)
+
+        if self.FightButtonMenu:
+            EMOTIONAL_ATTACK_BUTTON.draw(surface)
+            ATTACK_BUTTON.draw(surface)
+
+        ## EVENTS ##
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:                                                                               # exit game function
+                self.exit_battle()
+                self.game.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and not self.timer.active and not self.action:
+                if SURRENDER_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.mixer.Sound.play(MENU_SOUND)
                     self.exit_battle()
-                    self.game.running = False
-                if event.type == pygame.MOUSEBUTTONDOWN and not timer.active and not action:
-                    if SURRENDER_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        pygame.mixer.Sound.play(MENU_SOUND)
-                        self.exit_battle()
-                    if ITEMS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        items_count = len(self.game.inventory.items)
-                        if items_count > 0:
-                            random_index = random.randint(0, (items_count) - 1)
-                        else:
-                            random_index = None
-                        if random_index != None:
-#                            item = self.game.hud.items[random_index]
-                            #random_item_name = random.choice(self.game.hud.items)
-                            random_item = self.game.hud.items[random_index]
-                            random_item_name = random_item.name
-
-                            random_item.random_abilities()
-
-                            if self.game.debug:
-                                print(f"Item used: {random_item_name}")
+                if ITEMS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    self.current_display = "items_menu"    
 
 
-                    if FIGHT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        FightButtonMenu = True
-                    if FightButtonMenu:
-                        if EMOTIONAL_ATTACK_BUTTON.checkForInput(MENU_MOUSE_POS):
-                            self.attack_enemy(self.attack_type[1])
-                            EMOTIONAL_ATTACK_BUTTON.set_image(START_IMG_PRESSED, scale=0.4)
-                            action = "Enemy_Fight"                                                                          # after player's fight, set action variable that corresponds to fight for NPC_ENEMY
-                        elif ATTACK_BUTTON.checkForInput(MENU_MOUSE_POS):
-                            self.attack_enemy(self.attack_type[0])
-                            ATTACK_BUTTON.set_image(START_IMG_PRESSED, scale=0.4)
-                            action = "Enemy_Fight"                                                                          # after player's fight, set action variable that corresponds to fight for NPC_ENEMY
+#                        items_count = len(self.game.inventory.items)
+#                        if items_count > 0:
+#                            random_index = random.randint(0, (items_count) - 1)
+#                        else:
+#                            random_index = None
+#                        if random_index != None:
+#                            #random_item = random.choice(self.game.hud.items)
+#                            random_item = self.game.hud.items[random_index]
+#                            random_item_name = random_item.name
+#
+#                            random_item.random_abilities()
+#
+#                            if self.game.debug:
+#                                print(f"Item used: {random_item_name}")
 
-            if action:
-                if action == "Enemy_Fight" and self.enemy_health > 0:
-                    if action_start_time == 0:
-                        action_start_time = pygame.time.get_ticks()
-                    elif pygame.time.get_ticks() - action_start_time >= action_duration:
-                        self.attack_player()
-                        action = None
-                        action_start_time = 0
-                else:
-                    pass
 
-            if self.show_player_damage_action:
-                self.show_player_damage(surface)
-            if self.show_enemy_damage_action:
-                self.show_enemy_damage(surface)
+                if FIGHT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    self.FightButtonMenu = True
 
-            if self.enemy_health <= 0:                                                      # if enemy is defeated:
-                if timer.active == False and not timer.is_finished:                         # if timer hasn't been started yet:
-                    timer.start(5000)                                                       # set timer for 'n' ms
-                timer.update()                                                              # update timer's state
-                if timer.is_finished:                                                       # if timer is finished
-                    self.game.player.level += 1                                             # increment player's level
-                    self.exit_battle()                                                      # exit battle_menu
+                if self.FightButtonMenu:
+                    if EMOTIONAL_ATTACK_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        self.attack_enemy(self.attack_type[1])
+                        EMOTIONAL_ATTACK_BUTTON.set_image(START_IMG_PRESSED, scale=0.4)
+                        action = "Enemy_Fight"                                                                          # after player's fight, set action variable that corresponds to fight for NPC_ENEMY
+                    elif ATTACK_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        self.attack_enemy(self.attack_type[0])
+                        ATTACK_BUTTON.set_image(START_IMG_PRESSED, scale=0.4)
+                        action = "Enemy_Fight"                                                                          # after player's fight, set action variable that corresponds to fight for NPC_ENEMY
+
+        if self.action:
+            if self.action == "Enemy_Fight" and self.enemy_health > 0:
+                if self.action_start_time == 0:
+                    self.action_start_time = pygame.time.get_ticks()
+                elif pygame.time.get_ticks() - self.action_start_time >= self.action_duration:
+                    self.attack_player()
+                    self.action = None
+                    self.action_start_time = 0
             else:
                 pass
 
-            if self.player_health <= 0:
-                self.player_health = 0
-                if timer.active == False and not timer.is_finished:
-                    timer.start(5000)
-                timer.update()
-                if timer.is_finished:
-                    self.exit_battle()
+        if self.show_player_damage_action:
+            self.show_player_damage(surface)
+        if self.show_enemy_damage_action:
+            self.show_enemy_damage(surface)
 
-            pygame.display.update()
+        if self.enemy_health <= 0:                                                      # if enemy is defeated:
+            if self.timer.active == False and not self.timer.is_finished:                         # if timer hasn't been started yet:
+                self.timer.start(5000)                                                       # set timer for 'n' ms
+            self.timer.update()                                                              # update timer's state
+            if self.timer.is_finished:                                                       # if timer is finished
+                self.game.player.level += 1                                             # increment player's level
+                self.exit_battle()                                                      # exit battle_menu
+        else:
+            pass
+        if self.player_health <= 0:
+            self.player_health = 0
+            if self.timer.active == False and not self.timer.is_finished:
+                self.timer.start(5000)
+            self.timer.update()
+            if self.timer.is_finished:
+                self.exit_battle()
+        pygame.display.update()
+
+    def items_menu(self, surface):
+        # INITIALIZING BUTTONS
+        EXIT_BUTTON = Button(150, WINDOW_HEIGHT - 250, scale=0.40, image=EXIT_IMG, hovered_image=EXIT_IMG_PRESSED)
+
+        # SETTING TEXT FOR MENU
+        heading_text = "Your Items"
+
+        # DEFINING TEXT VARIABLES
+        headingtext = HEADINGTEXT.render(heading_text, True, (0, 0, 0)).convert_alpha()  # render a Small Text
+        headingtextrect = headingtext.get_rect()                                                              # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
+        headingtextrect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 250)                                     # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
+
+        # DEFINING CONSTANT VARIABLES
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        # DRAWING ON THE SURFACE
+        surface.blit(BACKGROUND_IMG)
+        surface.blit(headingtext, headingtextrect)
+
+        ## DRAWING BUTTONS ##
+        for button in [EXIT_BUTTON]:                     # iterate through every single button instance and draw it to the screen
+            button.draw(surface)
+
+        ## EVENTS ##
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:                                                                               # exit game function
+                self.exit_battle()
+                self.game.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if EXIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    self.current_display = "battle_menu"
+        pygame.display.update()
 
     def exit_battle(self):
         self.game.player.health = self.player_health
         self.game.npc_enemy.health = self.enemy_health
-        self.running = False
+        self.is_running = False
