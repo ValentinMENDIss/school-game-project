685800d388bfacd695823966f9e095f00aa43b2b
ValentinMENDIss <valiksuper58@gmail.com>
2025-03-07 22:12:13 +0100

    Added new map(neighbourhood) with collisions and objects

--- code/main.py
+++ code/main.py
@@ -76,7 +76,8 @@ class Game:
     def import_assets(self):
         self.tmx_maps = {'world': load_pygame(os.path.join('..', 'data', 'maps', 'world.tmx')),                         # load world.tmx file (with given location of it)
                          'world2': load_pygame(os.path.join('..', 'data', 'maps', 'world2.tmx')),
-                         'school-building-bottom-left': load_pygame(os.path.join('..', 'data', 'maps', 'school-building-bottom-left.tmx'))}
+                         'school-building-bottom-left': load_pygame(os.path.join('..', 'data', 'maps', 'school-building-bottom-left.tmx')),
+                         'neighbourhood': load_pygame(os.path.join('..', 'data', 'maps', 'neighbourhood.tmx'))}
 
     def setup(self, tmx_map, player_start_pos):
         # clear the map
