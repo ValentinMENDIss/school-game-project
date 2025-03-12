7ee70d841186f51b64b67f0b1329364ab71897a3
ValentinMENDIss <valiksuper58@gmail.com>
2025-03-12 10:19:49 +0100

    Shop(): added ability to buy items and get them in inventory

--- code/shop.py
+++ code/shop.py
@@ -5,24 +5,29 @@ from button import Button
 
 class Shop:
     def __init__(self, game):
+        self.debug = False
         self.game = game
         self.items_updated = False
         self.items = {
             "item-test": {
                 "name": "Apple",
                 "rarity": "RARE",
-                "price": 5.99
+                "price": 5.99,
+                "item_button": None
             },
             "item-test2": {
                 "name": "Water Bottle",
-                "rarity": "Epic",
-                "price": 1.99
+                "rarity": "EPIC",
+                "price": 1.99,
+                "item_button": None
             }
         }
+        self.action = None
 
     def display_shop(self, surface):
         self.running = True
         while self.running:
+            interacted_item = None
             MENU_MOUSE_POS = pygame.mouse.get_pos()
 
             # Display shop background
@@ -37,7 +42,15 @@ class Shop:
                 if event.type == pygame.QUIT:
                     self.running = False
                     self.game.current_screen = "game"
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for item in self.items:
+                        if self.items[item]["item_button"].checkForInput(MENU_MOUSE_POS):
+                            self.action = "buy_item"
+                            interacted_item = item
                     
+            if self.action == "buy_item":
+                self.buy_item(interacted_item)
+
             self.check_input()
                 
             pygame.display.update()
@@ -72,6 +85,16 @@ class Shop:
                 items_pos_x = items_pos_init_x + (100 * row_count)
                 items_pos_y = items_pos_init_y + (100 * column_count)
             button = Button(items_pos_x , items_pos_y, scale=1, image=item_image)
+            local_item["item_button"] = button
             button.draw(surface)
             row_count += 1
         self.items_updated = True
+
+    
+    def buy_item(self, item):
+        created_item = Items((0, 0), item, self.game.all_sprites, rarity=self.items[item]["rarity"], game=self.game)
+        self.game.inventory.add_item(created_item) 
+        self.items.pop(item)
+        self.action = None
+        if self.debug:
+            print(f"Inventory: {self.game.inventory}\nShop Items: {self.items}")
