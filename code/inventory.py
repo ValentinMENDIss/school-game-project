6c164a65d0e0a8a6ac3193cc6a8746ac49ccf4e7
ValentinMENDIss <valiksuper58@gmail.com>
2025-03-16 12:39:47 +0100

    Inventory: showing item's info when hovered on item button

--- code/inventory.py
+++ code/inventory.py
@@ -6,6 +6,7 @@ from timer import Timer
 
 ######### CLASSes ##############
 
+
 class Inventory:
     def __init__(self, game):
         self.items = []
@@ -65,7 +66,7 @@ class Inventory:
                     for item_button in self.items_dict:
                         if item_button.checkForInput(MENU_MOUSE_POS):
                             item = self.items_dict[item_button]["item"]
-                            self.item_get_info(item)
+                            self.item_show_info(surface, MENU_MOUSE_POS, item)
             for item_button in self.items_dict:
                 if item_button.checkForInput(MENU_MOUSE_POS):     
                     item_button.hovered_on = True
@@ -74,7 +75,7 @@ class Inventory:
                     self.timer.update()                                                              # update timer's state
                     if self.timer.is_finished:                                                       # if timer is finished
                         item = self.items_dict[item_button]["item"]
-                        self.item_get_info(item)
+                        self.item_show_info(surface, MENU_MOUSE_POS, item)
                 else:
                     if item_button.hovered_on:
                         self.timer.stop(loop=True)
@@ -83,7 +84,7 @@ class Inventory:
                 
  
  
-            self.game.clock.tick(15)
+            self.game.clock.tick(60)
             pygame.display.update()
 
     def __draw_items(self, surface, items_pos_init_x, items_pos_init_y, max_length=(WINDOW_WIDTH - 200)):
@@ -106,5 +107,29 @@ class Inventory:
             button.draw(surface)
         self.initialized = True
             
-    def item_get_info(self, item):
-        print(f"Item's information. Name: {item.name}, Rarity: {item.rarity}")
+    def item_show_info(self, surface, MENU_MOUSE_POS, item):
+        info_text = f"{item.name}\n{item.rarity}"
+        # Initialize variables to store the total height and maximum width
+        text_height = 0
+        text_width = 0
+
+        lines = info_text.splitlines()                                                                                   # Split the text into lines
+        for line in lines:
+            width, height = SMALLTEXT.size(line)
+            text_height += height
+            if width > text_width:
+                text_width = width
+            
+        SMALLTEXT.set_bold(info_text)
+        infotext = SMALLTEXT.render(info_text, True, (0, 0, 0)).convert_alpha()                                     # render a Small Text
+        infotextrect = infotext.get_rect()                                                                          # get a Rectangle of the small Text ( needed, to be able to place the text precisely )
+        
+        mouse_x_pos = MENU_MOUSE_POS[0]
+        mouse_y_pos = MENU_MOUSE_POS[1]
+        offset_x = 15
+        offset_y = 15
+
+        infotextrect.topleft = (mouse_x_pos + offset_x, mouse_y_pos + offset_y)                                     # Place a Text in the Center of the screen ( X-Coordinates ) and Bottom of the screen ( Y-Coordinates )
+
+        pygame.draw.rect(surface, (226, 233, 236), (mouse_x_pos, mouse_y_pos, text_width + (2*offset_x), text_height + (2*offset_y)))
+        surface.blit(infotext, infotextrect)
